40c988c941437cad1c3122efd3c01689acacd00a
Jagjeet Saini <jagjeetsaini@Jagjeets-MacBook-Air.local>
2023-04-28 14:09:46 +0530

    second commit

--- unique-1.js
+++ unique-1.js
@@ -10,21 +10,23 @@ const inputB = [1, 2, 3, 4, 5, 1, 7, 6, 4, 3, 8];
 const inputA = [1, 2, 3, 4, 5, 1];
 const inputB = [1, 2, 3, 4, 5, 1, 7, 6, 4, 3, 8];
 
-function unique_arr(inputA,inputB){
-    const output = [...inputA, ...inputB];
-    for (let i=0; i<output.length-1; i++){
-        //inputA[i]
-        for (let j=i+1; j<output.length; j++){
-            if(output[i] === output[j]){
-                output.slice[j];
-
-
-            }
-
-        }
-    console.log(output);    
+function unique_arr(inputA, inputB) {
+  const output = [...inputA, ...inputB];
+  for (let i = 0; i < output.length - 1; i++) {
+    for (let j = i + 1; j < output.length; j++) {
+      if (output[i] === output[j]) {
+        output.splice(j, 1);
+        j--;
+      }
     }
+  }
+  console.log(output);
 }
+
+unique_arr(inputA, inputB); 
+
+
+
 
 
 
